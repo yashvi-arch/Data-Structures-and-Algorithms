2df07c0d6f36bad2cbdc2e8c8420af217efb1d24
yashvi-arch <pandeyyashvi5@gmail.com>
2026-08-06 16:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (16.06%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -3,15 +3,15 @@ public:
     int minMutation(string startGene, string endGene, vector<string>& bank) {
         unordered_set<string>bankset(bank.begin(),bank.end());
         unordered_set<string>visited;
-        queue<string>que;
-        que.push(startGene);
+        queue<string>q;
         visited.insert(startGene);
+        q.push(startGene);
         int level=0;
-        while(!que.empty()){
-            int n=que.size();
+        while(!q.empty()){
+            int n=q.size();
             while(n--){
-                string curr=que.front();
-                que.pop();
+                string curr=q.front();
+                q.pop();
                 if(curr==endGene){
                     return level;
                 }
@@ -19,14 +19,12 @@ public:
                     for(int i=0;i<curr.size();i++){
                         string next=curr;
                         next[i]=ch;
-
                         if(visited.find(next)==visited.end() && bankset.find(next)!=bankset.end()){
                             visited.insert(next);
-                            que.push(next);
+                            q.push(next);
                         }
                     }
                 }
-
             }
             level++;
         }
