e541d1297f1adfdf6a135a1729f22a7885a4674e
yashvi-arch <pandeyyashvi5@gmail.com>
2026-08-10 01:21:28 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (97.84%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -4,15 +4,15 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         if(row<0 || row>=m || col<0 || col>=n || grid[row][col]==0){
-            return 0;
+            return 0; //out of bound case
         }
-        int count=1;
+        int count=1; //there is a 1 already so count is initialised as 1 already
         grid[row][col]=0;
         count+=dfs(grid,row+1,col);
         count+=dfs(grid,row-1,col);
         count+=dfs(grid,row,col+1);
         count+=dfs(grid,row,col-1);
-        return count;
+        return count; //returns the no of 1's present in all the four directions
         
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
@@ -22,7 +22,7 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    ans=max(ans,dfs(grid,i,j));
+                    ans=max(ans,dfs(grid,i,j)); //calc max area of 1's in the entire grid
                     
                 }
             }
