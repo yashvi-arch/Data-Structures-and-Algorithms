28c429af85844195adf1fc18d30eb25991ea80c8
yashvi-arch <pandeyyashvi5@gmail.com>
2026-08-23 13:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (12.89%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int>pq;
+        priority_queue<int>pq; //maxheap
+
         for(int i=0;i<stones.size();i++){
-            pq.push(stones[i]);
+            pq.push(stones[i]); //populate pq
         }
         while(pq.size()>1){
             int x=pq.top();
@@ -14,12 +15,17 @@ public:
 
             if(x!=y){
                 pq.push(abs(x-y));
-            }
+            }else{
+                continue;
+           }
         }
         if(pq.empty()){
             return 0;
+        }else{
+            return pq.top();
         }
-        return pq.top();
+        
+
         
     }
 };
