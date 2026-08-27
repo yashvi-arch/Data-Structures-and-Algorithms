2eb040703e088d2621c1f595e2a2d37fd4dd45b9
yashvi-arch <pandeyyashvi5@gmail.com>
2026-08-27 19:33:36 +0530

    Time: 59 ms (93.86%) | Memory: 11.4 MB (85.98%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int M=1e9+7;
+    typedef pair<int,int>p;
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int>temp;
+        priority_queue<p,vector<p>,greater<p>>pq;
         for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                temp.push_back(sum);
+            pq.push({nums[i],i});
+        }
+        int res=0;
+        for(int cnt=1;cnt<=right;cnt++){
+            auto v=pq.top();
+            pq.pop();
+            int sum=v.first;
+            int idx=v.second;
+
+            if(cnt>=left){
+                res=(res+sum)%M;
+            }
+            int next_idx=idx+1;
+            p next_pair; //new pair to be pushed
+            if(next_idx<n){ //not out of bound h
+                next_pair.first=sum+nums[next_idx];
+                next_pair.second=next_idx;
+                pq.push(next_pair);
             }
         }
-        sort(temp.begin(),temp.end());
-        int ans=0;
-        for(int i=left-1;i<=right-1;i++){
-            ans=(ans+temp[i])%M;
+        return res;
 
-        }
-        return ans;
         
     }
 };
