183ba52469ca2ccaaed4ee88e81d09316c3fd4b7
yashvi-arch <pandeyyashvi5@gmail.com>
2026-09-09 23:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (19.40%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int n=accounts.size();
+        int m=accounts[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int wealth=0;
+            for(int j=0;j<m;j++){
+                wealth+=accounts[i][j];
+            }
+            ans=max(ans,wealth);
+        } 
+        return ans;
+    }
+};
